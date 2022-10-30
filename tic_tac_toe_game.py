4ef52c2bb716b011ef95d9d11e4210d089df0c64
Krishanu-06 <116491458+Krishanu-06@users.noreply.github.com>
2022-10-30 16:26:41 +0530

    Update tic_tac_toe_game.py

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -22,8 +22,8 @@ def draw_board(positions):
                 print("-",end="")
         print("\n")
 
-# Create a data structure that holds the position 
-positions = {    
+# Create a data structure that holds the position
+positions = {
     (0,1) : 1,
     (0,5) : 2,
     (0,9) : 3,
@@ -54,18 +54,74 @@ if(user_status == 'Y' or user_status == 'y'):
     is_playing = True
 user = True
 
-while(is_playing):
+# Check if game is over
+def check_win(is_playing):
+    # Diagonal check
+    d1 = positions[mapping_positions[1]]
+    d2 = positions[mapping_positions[5]]
+    d3 = positions[mapping_positions[9]]
+    d4 = positions[mapping_positions[3]]
+    d5 = positions[mapping_positions[5]]
+    d6 = positions[mapping_positions[7]]
+    if d1 == d2 and d1 == d3:
+        is_playing = False
+        return is_playing
+    if d4 == d5 and d4 == d6:
+        is_playing = False
+        return is_playing
+
+    no_status = False
+    for i in range(9):
+        index = i + 1
+        if(index == 1 or index == 4 or index == 7) and (positions[mapping_positions[index]] == positions[mapping_positions[index + 1]]) and (positions[mapping_positions[index]] == positions[mapping_positions[index + 2]]):
+            is_playing = False
+            return is_playing
+        if (index == 1 or index == 2 or index == 3) and (positions[mapping_positions[index]] == positions[mapping_positions[index + 3]]) and (positions[mapping_positions[index]] == positions[mapping_positions[index + 6]]):
+            is_playing = False
+            return is_playing
+        if(positions[mapping_positions[index]] != "X" or positions[mapping_positions[index]] != "O"):
+            no_status = True
+
+    if(no_status == False):
+        is_playing = False
+    return is_playing
+
+# Check if input address is valid
+def is_valid_position(index):
+    is_valid = True
+    if index <= 0 or index > 9:
+        is_valid = False
+        return is_valid
+    for i in  positions:
+        if positions[i] == index:
+            is_valid = False
+            return is_valid
+    return is_valid
+
+
+def maintain_game(user):
     if(user == True):
         user_game = "X"
     else:
-        user_game = "O" 
+        user_game = "O"
     print("\n")
     draw_board(positions)
-    print("User ",user_game,"enter the index where you want to input by looking at the board")
+    print("User ",user_game," enter the index where you want to input by looking at the board")
     index = int(input())
-    if(index < 0 or index > 9):
+    is_valid = is_valid_position(index)
+    if(is_valid):
         print("Enter valid index")
     else:
         change_game_status = mapping_positions[index]
         positions[change_game_status] = user_game
-        user = not user 
+        user = not user
+    return user
+
+# Game loop
+while(is_playing):
+    if(check_win(is_playing)):
+        user = maintain_game(user)
+    else:
+        draw_board(positions)
+        print("Game Over \n")
+        break
